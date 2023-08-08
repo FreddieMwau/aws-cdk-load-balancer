05e42e981b6438a100e9611eb2b22c584b2940ec
Fredrick Mutua <fredrick.mutua@thejitu.com>
2023-08-08 12:54:26 +0300

    fix(stacks): reconfigure the stacks

--- bin/cdk-app.ts
+++ bin/cdk-app.ts
@@ -4,7 +4,12 @@ import * as cdk from 'aws-cdk-lib';
 import { CdkAppStack } from '../lib/cdk-app-stack';
 
 const app = new cdk.App();
-new CdkAppStack(app, 'CdkAppStack', {
+new CdkAppStack(app, 'cdk-stack-dev', {
+  stackName: 'cdk-stack-dev',
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  }
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
